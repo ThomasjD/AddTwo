6881245962297cf2c3b02f49d37d321290a6f2c8
Thomas Dinh <thomasdinh@Thomass-MBP.localdomain>
2018-05-01 15:29:40 -0500

    try1

---
+++ add.py
@@ -0,0 +1,9 @@
+#Take 2 float numbers as input and add it
+#round it to 2 decimal points
+
+#User input
+num1 = float(input("Enter 1st number!"))
+num2 = float(input("Enter 2nd number!"))
+
+#Sum
+print("Sum of 2 numbers is " + round(num1 + num2, 2))
